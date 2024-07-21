4ff510c7df49b0356dae62c9df4a1e6cc77f6446
Dilawar Singh <dilawar@subcom.tech>
2024-07-22 01:45:00 +0530

    chore: CStr based linkage

--- steve/src/lib.rs
+++ steve/src/lib.rs
@@ -2,5 +2,13 @@ use std::ffi::CStr;
 
 #[no_mangle]
 pub extern "C" fn strisnumber(txt: &CStr) -> i32 {
-    txt.to_str().map(|x| x.parse::<f64>()).is_ok().into()
+    txt.to_string_lossy().parse::<f64>().is_ok().into()
+}
+
+#[no_mangle]
+pub extern "C" fn strhasname(txt: &CStr, name: &CStr) -> i32 {
+    txt.to_string_lossy()
+        .as_ref()
+        .contains(name.to_string_lossy().as_ref())
+        .into()
 }
